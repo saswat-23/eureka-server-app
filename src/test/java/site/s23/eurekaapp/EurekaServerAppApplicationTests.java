06bf05e6cec61a894547399e06d53cf6e230e1a6
Saswat Mohanty <saswat23@gmail.com>
2024-11-03 21:33:38 +0530

    Eureka Server App - Initial Version

---
+++ src/test/java/site/s23/eurekaapp/EurekaServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package site.s23.eurekaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
